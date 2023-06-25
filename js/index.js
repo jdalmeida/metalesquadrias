8fe332e6b58d9ebd9f3f5286637c0a6a8c1c7b2f
cocacolacomvodka <joaogabrielde.almeida@hotmail.com>
2023-06-25 14:43:37 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var imagens = [
-      "./images/1.jpg",
-      "./images/2.jpg",
-      "./images/3.jpg",
-      "./images/4.jpg",
-      "./images/5.jpg",
-      "./images/6.jpg",
-      "./images/7.jpg"
+      "/images/1.jpg",
+      "/images/2.jpg",
+      "/images/3.jpg",
+      "/images/4.jpg",
+      "/images/5.jpg",
+      "/images/6.jpg",
+      "/images/7.jpg"
     ];
     
     var div = document.getElementById("logo");
