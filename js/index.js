eb311eb724f189c708e0ea483ccf222ea9727c90
cocacolacomvodka <joaogabrielde.almeida@hotmail.com>
2023-06-25 14:32:04 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var imagens = [
-      "../public/1.jpg",
-      "../public/2.jpg",
-      "../public/3.jpg",
-      "../public/4.jpg",
-      "../public/5.jpg",
-      "../public/6.jpg",
-      "../public/7.jpg"
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg"
     ];
     
     var div = document.getElementById("logo");
