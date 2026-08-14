24e1e5828dadca92104e03c47e6bee0c3e14e6fe
jdalmeida <joaogabrielde.almeida@hotmail.com>
2026-08-14 10:35:32 -0300

    add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -43,6 +43,22 @@ export const Route = createRootRoute({
 				rel: "stylesheet",
 				href: appCss,
 			},
+			{
+				rel: "icon",
+				type: "image/svg+xml",
+				href: "/favicon.svg",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
 		],
 		scripts: [
 			{
